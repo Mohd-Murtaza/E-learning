6c17e760d7d157336eac44debf489fea9dbe2591
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2024-02-25 17:32:38 +0530

    user model define done

--- E-learning_server/index.js
+++ E-learning_server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const PORT=process.env.PORT;
 const express=require("express");
-const connection = require("./config/db");
+const {connection} = require("./config/db");
 const app=express();
 
 app.get("/", (req,res)=>{
