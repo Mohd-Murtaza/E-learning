6ea4ac1fefee8b5a789644243ac2caaa6118f3dd
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2024-02-25 13:24:39 +0530

    database configuration done

--- E-learning_server/index.js
+++ E-learning_server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const PORT=process.env.PORT;
 const express=require("express");
+const connection = require("./config/db");
 const app=express();
 
 app.get("/", (req,res)=>{
@@ -11,6 +12,7 @@ app.use((req,res)=>{
     res.status(404).send("this is a invalid request");
 });
 
-app.listen(PORT, async()=>{
+app.listen(PORT, ()=>{
+    connection.then((res)=>console.log(`db is connected`)).catch((err)=>console.log(err))
     console.log(`server is runngin on this => ${PORT}`)
 })
