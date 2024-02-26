0a082c920e0e600e2422fa6548edfc1324d3c0f4
unknown <lalwadiyashubham123@gmail.com>
2024-02-26 12:01:25 +0530

    user/controller/login route is created and all necessary imports is also done for login route

---
+++ E-learning_server/controllers/user.controller.js
@@ -0,0 +1,35 @@
+const { UserModel } = require("../models/user.model");
+
+const userLogin=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const cookiesOption={
+            httpOnly:true,
+            secure:true,
+            sameSite:"none"
+        }
+        //user login email is registered or not checking
+        const findUserWithMail=await UserModel.findOne({email});
+        if(!findUserWithMail){
+           return res.status(401).send({msg:"you have to put your email that you register"});
+        }
+        //if email is ok ( password checking )
+        const passwordChecking= await UserModel.comparePassword(password);
+        if(!passwordChecking){
+            return res.status(401).send({msg:"your password is wrong"});
+        }
+        //if password is correct than we have to generate access_token and refresh_token
+        const accesstoken=await UserModel.generateAccessToken();
+        const refreshtoken=await UserModel.generateAccessToken();
+
+        res.cookie("accesstoken",accesstoken,cookiesOption);
+        res.cookie("refreshtoken",refreshtoken,cookiesOption);
+
+        //user is successfully login
+        res.status(200).send({status:"successfull",msg:"user is successfully login"})
+    } catch (error) {
+        res.status(400).send({status:"error while user login",msg:error.message});
+    }
+}
+
+module.exports={userLogin}
--- E-learning_server/index.js
+++ E-learning_server/index.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const PORT=process.env.PORT;
 const express=require("express");
+const cookieParser = require("cookie-parser");
 const {connection} = require("./config/db");
 const { userRouter } = require("./routes/user.route");
 const app=express();
 
+
+//all middleware
+app.use(express.json())
+app.use(cookieParser())
+
+//all routes
 app.use("/users",userRouter);
 
 app.get("/", (req,res)=>{
