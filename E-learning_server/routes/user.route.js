54a6a66a5c63213fcb5a2c4712176a8c595506b4
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2024-03-04 09:35:17 +0530

    user registretion done

--- E-learning_server/routes/user.route.js
+++ E-learning_server/routes/user.route.js
@@ -1,9 +1,12 @@
 const express=require("express");
-const { userLogin } = require("../controllers/user.controller");
+const { userLogin, userRegister, userVerifyOTP, userResendOTP } = require("../controllers/user.controller");
 const userRouter=express.Router();
 
 
-userRouter.post("/login",userLogin)
+userRouter.route("/login").post(userLogin);
+userRouter.route("/register").post(userRegister);
+userRouter.route("/verifyotp").post(userVerifyOTP);
+userRouter.route("/resendotp").post(userResendOTP);
 
 
 module.exports={userRouter};
