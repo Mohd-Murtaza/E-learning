4545bebf6353e85b16dbc768d74b84dfac86aa3d
vijendra2244 <vs230267@gmail.com>
2024-03-07 13:40:19 +0530

    Done forget and reset password functionality

--- E-learning_server/src/routes/user.route.js
+++ E-learning_server/src/routes/user.route.js
@@ -1,11 +1,23 @@
-const express=require("express");
-const { userLogin, userRegister, userVerifyOTP, userResendOTP, gitRegistration } = require("../controllers/user.controller");
-const userRouter=express.Router();
-
+const express = require("express");
+const {
+  userLogin,
+  userRegister,
+  userVerifyOTP,
+  userResendOTP,
+  gitRegistration,
+  resetPassword,
+  requestForOtpToForgetPass,
+  forgetPassword,
+} = require("../controllers/user.controller");
+const userRouter = express.Router();
 
 userRouter.route("/login").post(userLogin);
 userRouter.route("/register").post(userRegister);
-userRouter.route("/verifyotp").post(userVerifyOTP);
-userRouter.route("/resendotp").post(userResendOTP);
-userRouter.route("/gitRegistration").get(gitRegistration)
-module.exports={userRouter};
+userRouter.route("/verifyOtp").post(userVerifyOTP);
+userRouter.route("/resendOtp").post(userResendOTP);
+userRouter.route("/gitRegistration").get(gitRegistration);
+userRouter.route("/resetPassword").post(resetPassword);
+userRouter.route("/otpRequestForPass").post(requestForOtpToForgetPass);
+userRouter.route("/forgetPassword").post(forgetPassword);
+
+module.exports = { userRouter };
