50212c0c50eee42825bdb0d14f4b3dd7c803a7d7
vijendra2244 <vs230267@gmail.com>
2024-03-12 08:54:39 +0530

    Upload files on cloudinary is completed

---
+++ E-learning_server/src/utils/cloudinary.utils.js
@@ -0,0 +1,28 @@
+const cloudinary = require(cloudinary).v2;
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// CLOUDINARY  configuration
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localPath) return null;
+    const response = cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+module.exports = { uploadOnCloudinary };
